99d8675f72953f0e9c6823daecb55dbdc7391fb8
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-17 02:51:37 -0500

    move variables into generatePassword function
    moved some the variables that were global in the class-time code and
    mmoved them into the generatePassword function. This solves some issues
    including the string for usable characters not reseting, and I changed
    the passwrod variable to passWord in the function to avoid future
    conflict with the password variable in writePassword function. Also,
    moving the variables inside the funciton means that the prompts only
    show when the button is pressed, which is both dope and the intended
    behavior.

--- script.js
+++ script.js
@@ -7,142 +7,144 @@ var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', '
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\', ';', ':', "'", '"', ',', '.', '/', '<', '>', '?'];
 
-var characterSet = [];
-var passwordLength = +prompt('Please select password length');
-var password = '';
-console.log(passwordLength);
-
-var wantsLowercase = true;
-var wantsUppercase = false;
-var wantsNumbers = false;
-var wantsSpecialChar = false;
-
 // Function to generate password
 function generatePassword() {
 
+  var characterSet = [];
+  var passwordLength = +prompt('Please select password length');
+  var passWord = '';
+  console.log(passwordLength);
+
+  var wantsLowercase = true;
+  var wantsUppercase = true;
+  var wantsNumbers = false;
+  var wantsSpecialChar = false;
+
   //Let the user select character sets
+  console.log(characterSet)
 
   if (wantsLowercase) {
-      characterSet = characterSet.concat(lowercaseLetters);
+    characterSet = characterSet.concat(lowercaseLetters);
   }
   console.log(characterSet);
   if (wantsUppercase) {
-      characterSet = characterSet.concat(uppercaseLetters);
+    characterSet = characterSet.concat(uppercaseLetters);
   }
 
   if (wantsNumbers) {
-      characterSet = characterSet.concat(numbers);
+    characterSet = characterSet.concat(numbers);
   }
 
   if (wantsSpecialChar) {
-      characterSet = characterSet.concat(specialCharacters);
+    characterSet = characterSet.concat(specialCharacters);
   }
 
   //Generate the password
 
   do {
     for (var count = 0; count < passwordLength; count++) {
-        var randIndex = Math.floor(Math.random() * characterSet.length);
-        password += characterSet[randIndex];
+      var randIndex = Math.floor(Math.random() * characterSet.length);
+      passWord += characterSet[randIndex];
     }
-  
-    //Check password meets requirements
-  } while (!checkPassword());
 
-  console.log('Your new password: \n' + password);
-  return password;
+    //Check password meets requirements
+  } while (!checkPassword(passWord, wantsLowercase, wantsUppercase, wantsNumbers, wantsLowercase));
+
+  console.log('Your new password: \n' + passWord);
+  return passWord;
 }
 
 //Check that each prefered type is in the password
 //if wantsType == true, check there is one character of the type in the password
 
-function checkPassword() {
+function checkPassword(pword, lowercase, uppercase, num, specChar) {
   var hasReqs = true; //change to false if missing requirements
   var intersection;
   var alpha;
-  
-  if (wantsLowercase) {
-      for (var i = 0; i < lowercaseLetters.length; i++) {
-          alpha = password.includes(lowercaseLetters[i])
-          console.log(alpha)
-          if (alpha) {
-              if (intersection === undefined) {
-                  intersection = lowercaseLetters[i];
-                  console.log(intersection);
-              } else {
-                  intersection += lowercaseLetters[i];
-                  console.log(intersection);
-              }
-              
-          }
+  var passPlaceholder = pword;
+
+  if (lowercase) {
+    for (var i = 0; i < lowercaseLetters.length; i++) {
+      alpha = passPlaceholder.includes(lowercaseLetters[i])
+      console.log(alpha)
+      if (alpha) {
+        if (intersection === undefined) {
+          intersection = lowercaseLetters[i];
+          console.log(intersection);
+        } else {
+          intersection += lowercaseLetters[i];
+          console.log(intersection);
+        }
+
       }
-      if (intersection === undefined) {
-          hasReqs = false;
-      }
-      console.log("Has lowercase: " + hasReqs);
+    }
+    if (intersection === undefined) {
+      hasReqs = false;
+    }
+    console.log("Has lowercase: " + hasReqs);
   }
-  
-  if (hasReqs && wantsUppercase) {
-      for (var i = 0; i < uppercaseLetters.length; i++) {
-          alpha = password.includes(uppercaseLetters[i])
-          console.log(alpha)
-          if (alpha) {
-              if (intersection === undefined) {
-                  intersection = uppercaseLetters[i];
-                  console.log(intersection);
-              } else {
-                  intersection += uppercaseLetters[i];
-                  console.log(intersection);
-              }
-              
-          }
+
+  if (hasReqs && uppercase) {
+    for (var i = 0; i < uppercaseLetters.length; i++) {
+      alpha = passPlaceholder.includes(uppercaseLetters[i])
+      console.log(alpha)
+      if (alpha) {
+        if (intersection === undefined) {
+          intersection = uppercaseLetters[i];
+          console.log(intersection);
+        } else {
+          intersection += uppercaseLetters[i];
+          console.log(intersection);
+        }
+
       }
-      if (intersection === undefined) {
-          hasReqs = false;
-      }
-      console.log("Has uppercase: " + hasReqs);
+    }
+    if (intersection === undefined) {
+      hasReqs = false;
+    }
+    console.log("Has uppercase: " + hasReqs);
   }
-  
-  if (hasReqs && wantsNumbers) {
-      for (var i = 0; i < numbers.length; i++) {
-          alpha = password.includes(numbers[i])
-          console.log(alpha)
-          if (alpha) {
-              if (intersection === undefined) {
-                  intersection = numbers[i];
-                  console.log(intersection);
-              } else {
-                  intersection += numbers[i];
-                  console.log(intersection);
-              }
-              
-          }
+
+  if (hasReqs && num) {
+    for (var i = 0; i < numbers.length; i++) {
+      alpha = passPlaceholder.includes(numbers[i])
+      console.log(alpha)
+      if (alpha) {
+        if (intersection === undefined) {
+          intersection = numbers[i];
+          console.log(intersection);
+        } else {
+          intersection += numbers[i];
+          console.log(intersection);
+        }
+
       }
-      if (intersection === undefined) {
-          hasReqs = false;
-      }
-      console.log("Has numbers: " + hasReqs);
+    }
+    if (intersection === undefined) {
+      hasReqs = false;
+    }
+    console.log("Has numbers: " + hasReqs);
   }
-  
-  if (hasReqs && wantsSpecialChar) {
-      for (var i = 0; i < specialCharacters.length; i++) {
-          alpha = password.includes(specialCharacters[i])
-          console.log(alpha)
-          if (alpha) {
-              if (intersection === undefined) {
-                  intersection = specialCharacters[i];
-                  console.log(intersection);
-              } else {
-                  intersection += specialCharacters[i];
-                  console.log(intersection);
-              }
-              
-          }
+
+  if (hasReqs && specChar) {
+    for (var i = 0; i < specialCharacters.length; i++) {
+      alpha = passPlaceholder.includes(specialCharacters[i])
+      console.log(alpha)
+      if (alpha) {
+        if (intersection === undefined) {
+          intersection = specialCharacters[i];
+          console.log(intersection);
+        } else {
+          intersection += specialCharacters[i];
+          console.log(intersection);
+        }
+
       }
-      if (intersection === undefined) {
-          hasReqs = false;
-      }
-      console.log("Has special characters: " + hasReqs);
+    }
+    if (intersection === undefined) {
+      hasReqs = false;
+    }
+    console.log("Has special characters: " + hasReqs);
   }
   return hasReqs
 }
