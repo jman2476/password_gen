673bfcce0e814fbad51d6c4e2413cc470b5cd505
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-17 14:30:48 -0500

    add input scrubber for passwordLength
    adds a function to gather the desired password length, and checks that
    it is between 8 and 128, inclusive. If the user passes a string, the
    string's length is taken as the input and then checked that it is
    between 8 and 128, inclusive. If the user hits cancel the first time, it
    will prompt once more, and cancelling again will exit the password
    generation task. If the user puts in a bad input first, and then cancels
    the second time they are prompted, it will again cancel the task.

--- script.js
+++ script.js
@@ -11,21 +11,29 @@ var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', 
 function generatePassword() {
 
   var characterSet = [];
-  var passwordLength = +prompt('Please select password length');
+  var passwordLength = readPassLength();
   var passWord = '';
   console.log(passwordLength);
 
+  // Verify a value has been passed for the password length
+  if (!passwordLength) {
+    alert('No value passed for password lenght, task canceled');
+    return;
+  }
+
+  // Gets desired character types from user
   var wantsLowercase = confirm('Do you want lowercase letters?');
   var wantsUppercase = confirm('Do you want uppercase letters?');
   var wantsNumbers = confirm('How about some numbers? Ya want some numbers?');
   var wantsSpecialChar = confirm('Maybe some special characters would be nice, how about that?');
 
-  do {
+  // Checks at least on data type has been selected
+  while (!wantsLowercase && !wantsUppercase && !wantsNumbers && !wantsSpecialChar) {
     wantsLowercase = confirm('You need to select at least one character type. Are you sure you don‘t want lowercase letters?');
     wantsUppercase = confirm('You need to select at least one character type. Do you want uppercase letters?');
     var wantsNumbers = confirm('You need to select at least one character type. How about some numbers? Surely you want some numbers?');
     var wantsSpecialChar = confirm('You need to select at least one character type. Maybe some special characters would be nice, how about that? If you don’t want anything, I’m going to keep asking.');
-  } while (!wantsLowercase && !wantsUppercase && !wantsNumbers && !wantsSpecialChar);
+  }
 
   //Let the user select character sets
   console.log(characterSet)
@@ -33,7 +41,7 @@ function generatePassword() {
   if (wantsLowercase) {
     characterSet = characterSet.concat(lowercaseLetters);
   }
-  console.log(characterSet);
+
   if (wantsUppercase) {
     characterSet = characterSet.concat(uppercaseLetters);
   }
@@ -57,7 +65,6 @@ function generatePassword() {
     //Check password meets requirements
   } while (!checkPassword(passWord, wantsLowercase, wantsUppercase, wantsNumbers, wantsLowercase));
 
-  console.log('Your new password: \n' + passWord);
   return passWord;
 }
 
@@ -155,6 +162,31 @@ function checkPassword(pword, lowercase, uppercase, num, specChar) {
   }
   return hasReqs
 }
+
+// Function to scrub passwordLength input
+function readPassLength() {
+  var lengthDummy = prompt('Please select password length between 8 and 128 characters. If a number is not given, the length of your input string will be used.');
+  var scndDummy;
+
+  // Check if string given is a number or letters, and is between 8-128
+  do {
+    
+    if (isNaN(+lengthDummy)){
+      scndDummy = lengthDummy.length;
+    } else {
+      scndDummy = +lengthDummy;
+    }
+
+    if (scndDummy >= 8 && scndDummy <= 128){
+      lengthDummy = scndDummy;
+    } else {
+      lengthDummy = prompt('Your input did not meet the requirements. Please select password length between 8 and 128 characters. If a number is not given, the length of your input string will be used.');
+    }
+  } while (typeof lengthDummy === 'string')
+  console.log(lengthDummy)
+  return lengthDummy;
+}
+
 
 // Write password to the #password input
 function writePassword() {
