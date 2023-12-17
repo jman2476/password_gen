47521bef46f59f9e90c5ec8abdb984b78628055f
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-17 02:05:44 -0500

    Add password checker
    Adds the password checking function to verify the password has all
    required character types, and wraps the for loop that generates the
    password in a do-while loop that checks the password after each
    iteration. If the password has the requirements, it exits the loop.

--- script.js
+++ script.js
@@ -40,13 +40,111 @@ function generatePassword() {
 
   //Generate the password
 
-  for (var count = 0; count < passwordLength; count++) {
-      var randIndex = Math.floor(Math.random() * characterSet.length);
-      password += characterSet[randIndex];
-  }
+  do {
+    for (var count = 0; count < passwordLength; count++) {
+        var randIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randIndex];
+    }
+  
+    //Check password meets requirements
+  } while (!checkPassword());
 
   console.log('Your new password: \n' + password);
   return password;
+}
+
+//Check that each prefered type is in the password
+//if wantsType == true, check there is one character of the type in the password
+
+function checkPassword() {
+  var hasReqs = true; //change to false if missing requirements
+  var intersection;
+  var alpha;
+  
+  if (wantsLowercase) {
+      for (var i = 0; i < lowercaseLetters.length; i++) {
+          alpha = password.includes(lowercaseLetters[i])
+          console.log(alpha)
+          if (alpha) {
+              if (intersection === undefined) {
+                  intersection = lowercaseLetters[i];
+                  console.log(intersection);
+              } else {
+                  intersection += lowercaseLetters[i];
+                  console.log(intersection);
+              }
+              
+          }
+      }
+      if (intersection === undefined) {
+          hasReqs = false;
+      }
+      console.log("Has lowercase: " + hasReqs);
+  }
+  
+  if (hasReqs && wantsUppercase) {
+      for (var i = 0; i < uppercaseLetters.length; i++) {
+          alpha = password.includes(uppercaseLetters[i])
+          console.log(alpha)
+          if (alpha) {
+              if (intersection === undefined) {
+                  intersection = uppercaseLetters[i];
+                  console.log(intersection);
+              } else {
+                  intersection += uppercaseLetters[i];
+                  console.log(intersection);
+              }
+              
+          }
+      }
+      if (intersection === undefined) {
+          hasReqs = false;
+      }
+      console.log("Has uppercase: " + hasReqs);
+  }
+  
+  if (hasReqs && wantsNumbers) {
+      for (var i = 0; i < numbers.length; i++) {
+          alpha = password.includes(numbers[i])
+          console.log(alpha)
+          if (alpha) {
+              if (intersection === undefined) {
+                  intersection = numbers[i];
+                  console.log(intersection);
+              } else {
+                  intersection += numbers[i];
+                  console.log(intersection);
+              }
+              
+          }
+      }
+      if (intersection === undefined) {
+          hasReqs = false;
+      }
+      console.log("Has numbers: " + hasReqs);
+  }
+  
+  if (hasReqs && wantsSpecialChar) {
+      for (var i = 0; i < specialCharacters.length; i++) {
+          alpha = password.includes(specialCharacters[i])
+          console.log(alpha)
+          if (alpha) {
+              if (intersection === undefined) {
+                  intersection = specialCharacters[i];
+                  console.log(intersection);
+              } else {
+                  intersection += specialCharacters[i];
+                  console.log(intersection);
+              }
+              
+          }
+      }
+      if (intersection === undefined) {
+          hasReqs = false;
+      }
+      console.log("Has special characters: " + hasReqs);
+  }
+  return hasReqs
 }
 
 // Write password to the #password input
